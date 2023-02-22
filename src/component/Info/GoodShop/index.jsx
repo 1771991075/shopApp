8c323e229b12c7727d71eab14f672f06bcfe40bb
lzc <lvzicong123@outlook.com>
2023-02-22 15:49:02 +0800

    优品推荐

--- src/component/Info/GoodShop/index.jsx
+++ src/component/Info/GoodShop/index.jsx
@@ -5,31 +5,58 @@ import Lazyimg from 'react-lazyimg-component';
 import loading from '../../../assets/loading.png'
 
 export default class GoodShop extends Component {
-    render() {
+    state = {
+        goodShopList: []
+    }
+
+    componentDidMount() {
         const { goodShop } = this.props
+        let list = goodShop.list
+        //总数量
+        let sum = list.length
+        //总共几个元素
+        let num = Math.ceil(sum / 6)
+        let arry = []
+        for (let i = 0; i < num; i++) {
+            arry[i] = list.splice(0, 6)
+        }
+        this.setState({
+            goodShopList: arry
+        })
+    }
+
+    render() {
+        const { goodShopList } = this.state
         return (
             <div className='goodshop'>
                 <p className='yptj'>优品推荐</p>
 
                 <div className='goodshopswiper'>
-                    <Swiper autoplay={3000}>
-                        <Swiper.Item>
-                            <div className='goodshoplist'>
+                    {
+                        goodShopList.length !== 0 && (
+                            <Swiper autoplay={3000}>
                                 {
-                                    goodShop.list.map((item, index) => {
-                                        return <div key={index} className='goodshopitem'>
-                                            <div className='goodshopimg'><Lazyimg className="lazy" src={item.image} placeholder={<img src={loading} alt='加载失败' />} /></div>
-                                            <div className='goodshopname'>{item.storeName}</div>
-                                            <p>￥{item.price}</p>
-                                        </div>
+                                    goodShopList.map((item, index) => {
+                                        return (
+                                            <Swiper.Item key={index}>
+                                                <div className='goodshoplist'>
+                                                    {
+                                                        item.map((like, i) => {
+                                                            return <div key={i} className='goodshopitem'>
+                                                                <div className='goodshopimg'><Lazyimg className="lazy" src={like.image} placeholder={<img src={loading} alt='加载失败' />} /></div>
+                                                                <div className='goodshopname'>{like.storeName}</div>
+                                                                <p>￥{like.price}</p>
+                                                            </div>
+                                                        })
+                                                    }
+                                                </div>
+                                            </Swiper.Item>
+                                        )
                                     })
                                 }
-                            </div>
-                        </Swiper.Item>
-                        <Swiper.Item>
-                            <div><Lazyimg className="lazy111" src='' placeholder={<img src={loading} alt='加载失败' />} /></div>
-                        </Swiper.Item>
-                    </Swiper>
+                            </Swiper>
+                        )
+                    }
                 </div>
             </div>
         )
