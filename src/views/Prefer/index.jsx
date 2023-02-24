09214e295548d922eddba7a8d30356cd1b0c14ed
lzc <lvzicong123@outlook.com>
2023-02-24 16:45:34 +0800

    完善优惠券页面

--- src/views/Prefer/index.jsx
+++ src/views/Prefer/index.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import WithRouter from '../../router/withRouter'
-import { NavBar, Toast ,Empty} from 'react-vant';
+import { NavBar, Toast ,Empty ,Cell} from 'react-vant';
 import { ArrowLeft, Ellipsis,CouponO } from '@react-vant/icons'
+import {getUser} from '../../api/mine'
 import './index.css'
 
 class Prefer extends Component {
 
     state = {
         //优惠券
-        preferList: []
+        preferList: 0
     }
 
     render() {
@@ -29,18 +30,29 @@ class Prefer extends Component {
                 </div>
                 <div className='youhuiquan'>
                     {
-                        preferList.length === 0 ? <Empty
+                        preferList === 0 ? <Empty
                             imageSize={90}
                             image={<CouponO fontSize={90} color='#999' />}
                             description="暂无优惠券~~~" >
-                        </Empty> : <div></div>
+                        </Empty> : <div>
+                            <Cell title='优惠券' isLink value={`${preferList}张可用`} />
+                        </div>
                     }
                 </div>
             </div>
         )
     }
-    componentDidMount(){
-        
+    async componentDidMount(){
+        Toast.loading({
+            message: '加载中...',
+            forbidClick: true,
+        })
+        let res = await getUser()
+        this.setState({
+            preferList:res.data.data.couponCount
+        },()=>{
+            Toast.clear()
+        })
     }
 }
 
