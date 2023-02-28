673a7dfc70848f71cedb39ec49db53ff04171949
lzc <lvzicong123@outlook.com>
2023-02-28 19:11:49 +0800

    优化优惠券页面

--- src/api/mine.js
+++ src/api/mine.js
@@ -9,8 +9,16 @@ let sendImg = (data) =>sendHttp(`/api/front/upload/image?model=maintain&pid=0`,'
 //保存个人修改信息
 let saveUser = (data) => sendHttp(`/api/front/user/edit`,'post',data)
 
+//获取优惠券
+let getPrefer = ()=> sendHttp(`api/front/coupon/list?page=1&limit=20&type=usable`,'get')
+
+//获取过期的优惠券
+let getUnPrefer = () => sendHttp(`/api/front/coupon/list?page=1&limit=20&type=unusable`,'get')
+
 export {
     getUser,
     sendImg,
-    saveUser
+    saveUser,
+    getPrefer,
+    getUnPrefer
 }
--- src/views/Prefer/index.jsx
+++ src/views/Prefer/index.jsx
@@ -1,58 +1,104 @@
 import React, { Component } from 'react'
 import WithRouter from '../../router/withRouter'
-import { NavBar, Toast ,Empty ,Cell} from 'react-vant';
-import { ArrowLeft, Ellipsis,CouponO } from '@react-vant/icons'
-import {getUser} from '../../api/mine'
+import { Toast, Empty, Tabs } from 'react-vant';
+import { CouponO } from '@react-vant/icons'
+import { getPrefer, getUnPrefer } from '../../api/mine'
 import './index.css'
 
 class Prefer extends Component {
 
     state = {
         //优惠券
-        preferList: 0
+        preferList: [],
+        activeIndex:0
     }
 
     render() {
-        let { preferList } = this.state
+        let { preferList ,activeIndex } = this.state
         return (
             <div className='prefer'>
                 <div>
                     <div className='prefer_header'>
-                        <NavBar
-                            title='优惠券'
-                            leftText={<ArrowLeft fontSize={20} />}
-                            rightText={<Ellipsis fontSize={20} />}
-                            onClickLeft={() => this.props.router.navigate(-1)}
-                            onClickRight={() => Toast('更多')}
-                            fixed
-                        />
+                        <Tabs color={'#e93323'} onChange={(tabIndex)=>this.changeIndex(tabIndex)}>
+                            <Tabs.TabPane key={0} title={`未使用`}>
+                            </Tabs.TabPane>
+                            <Tabs.TabPane key={1} title={`已使用/过期`}>
+                            </Tabs.TabPane>
+                        </Tabs>
                     </div>
                 </div>
                 <div className='youhuiquan'>
                     {
-                        preferList === 0 ? <Empty
+                        preferList.length === 0 ? <Empty
                             imageSize={90}
                             image={<CouponO fontSize={90} color='#999' />}
                             description="暂无优惠券~~~" >
                         </Empty> : <div>
-                            <Cell title='优惠券' isLink value={`${preferList}张可用`} />
+                            {
+                                preferList.map((item, index) => {
+                                    return (
+                                        <div className='yhqitem' key={index}>
+                                            <div className='yhqitem1' style={{backgroundColor:activeIndex===0?'#e93323':'#a6a6a6'}}>
+                                                <div className='yhqleft'><span className='hb'>￥</span><span className='money'>{+item.money}</span></div>
+                                                <p className='manjian'>满{+item.minPrice}元可用</p>
+                                            </div>
+                                            <div className='yhqitem2'>
+                                                <div className='yhqright'><span className='tongyong'>通用</span><span>{item.name}</span></div>
+                                                <div className='yhqright2'>
+                                                    <div>{item.useStartTimeStr}~{item.useEndTimeStr}</div>
+                                                    <div className='keyong' style={{backgroundColor:activeIndex===0?'#e93323':'#a6a6a6',borderColor:activeIndex===0?'#e93323':'#a6a6a6'}}>{activeIndex===0?'可用':'过期'}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <div className='dixian'>我也是有底线的~</div>
                         </div>
                     }
                 </div>
             </div>
         )
     }
-    async componentDidMount(){
+
+    //改变索引时
+    changeIndex(tabIndex){
+        this.setState({
+            activeIndex:tabIndex
+        },()=>{
+            if(this.state.activeIndex===0){
+                this.getUse()
+            }else{
+                this.getUnUse()
+            }
+        })
+    }
+
+    //获取可用优惠券
+    async getUse() {
+        let res = await getPrefer()
+        this.setState({
+            preferList: res.data.data.list
+        }, () => {
+            Toast.clear()
+        })
+    }
+    //获取不可用优惠券
+    async getUnUse() {
+        let res = await getUnPrefer()
+        this.setState({
+            preferList: res.data.data.list
+        }, () => {
+            Toast.clear()
+        })
+    }
+
+    componentDidMount() {
         Toast.loading({
             message: '加载中...',
             forbidClick: true,
         })
-        let res = await getUser()
-        this.setState({
-            preferList:res.data.data.couponCount
-        },()=>{
-            Toast.clear()
-        })
+        this.getUse()
     }
 }
 
